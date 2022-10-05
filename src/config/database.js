0cec00241b26e0025859c36509346d213538af85
Clisciano <clisciano@gmail.com>
2022-10-05 18:29:25 -0300

    build

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,33 @@
-const config = {
-    dialect: 'mssql',
-    database: 'ctlab',
-    username: 'sa',
-    password: 'mppa2022',
-    host: 'localhost',
-    port: 1433,
-    logging: true,
-    options: {
-        trustedconnection: true,
-        enableArithAbort: true,
-        instancename: '',
-    },
+// const config = {
+//     dialect: 'mssql',
+//     database: 'ctlab',
+//     username: 'sa',
+//     password: 'mppa2022',
+//     host: 'localhost',
+//     port: 1433,
+//     logging: true,
+//     options: {
+//         trustedconnection: true,
+//         enableArithAbort: true,
+//         instancename: '',
+//     },
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//         underscoredAll: true,
+//     },
+// };
+
+// module.exports = config;
+module.exports = {
+    dialect: 'postgres',
+    host: 'ceus.postgres.uhserver.com',
+    username: 'clisciano',
+    password: '@Dmin681',
+    database: 'ceus',
     define: {
         timestamps: true,
         underscored: true,
         underscoredAll: true,
     },
 };
-
-module.exports = config;
