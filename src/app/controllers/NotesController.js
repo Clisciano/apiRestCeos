c5cedc3b2797d568ca02f2a1289726fb44b99f18
Clisciano <clisciano@gmail.com>
2022-12-07 16:56:01 -0300

    feature accordion Notes model controller migration routes database

---
+++ src/app/controllers/NotesController.js
@@ -0,0 +1,80 @@
+import Note from '../models/Notes.js';
+import PhysicalPerson from '../models/PhysicalPerson.js';
+
+class NoteController {
+    async store(req, res) {
+        const { id, descricao, person_pf_id } =
+            await Note.create(req.body);
+
+        return res.json({
+            id,
+            descricao,
+            person_pf_id,
+        });
+    }
+
+    async show(req, res) {
+        const { id } = req.params;
+
+        const person = await PhysicalPerson.findOne({
+            attributes: ['nome'],
+            where: { id },
+            include: [
+                {
+                    model: Note,
+                    where: { person_pf_id: id },
+                    attributes: ['id', 'descricao'],
+                },
+            ],
+        });
+
+        return res.json(person);
+    }
+
+    async index(req, res) {
+        const note = await Note.findAll({
+            include: [
+                {
+                    model: PhysicalPerson,
+                    attributes: ['id', 'nome'],
+                },
+            ],
+        });
+
+        return res.json(note);
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+
+        const { descricao } = req.body;
+
+        const noteExists = await Note.findOne({
+            where: { id },
+        });
+
+        if (!noteExists) {
+            return res
+                .status(400)
+                .json({ error: 'observation already exists.' });
+        }
+
+        const response = await noteExists.update({
+            descricao,
+        });
+
+        return res.json(response);
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const response = await Note.destroy({
+            where: { id },
+        });
+
+        return res.json(response);
+    }
+}
+
+export default new NoteController();
--- src/database/index.js
+++ src/database/index.js
@@ -7,6 +7,7 @@ import FilePhoto from '../app/models/FilePhoto.js';
 import FileSignature from '../app/models/FileSignature.js';
 import PhysicalPerson from '../app/models/PhysicalPerson.js';
 import Identity from '../app/models/Identity.js';
+import Notes from '../app/models/Notes.js';
 import Address from '../app/models/AddressPf.js';
 import Contact from '../app/models/Contact.js';
 import Vehicle from '../app/models/Vehicle.js';
@@ -38,6 +39,7 @@ const models = [
     Weapon,
     Infopen,
     Visitor,
+    Notes,
 ];
 
 class Database {
